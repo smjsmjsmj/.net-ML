5b488e3922fdd471cbc0a33eca0fa6f4ad784fdf
v-misun <v-misun@MININT-BPCM41C>
2018-07-03 12:38:32 +0800

    first commit code

---
+++ MyApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System;
+
+namespace myApp
+{
+    public class IrisData
+    {
+        [Column("0")]
+        public float SepalLength;
+
+        [Column("1")]
+        [ColumnName("Label")]
+        public string Label;
+
+        [Column("2")]
+        public string Name;
+    }
+
+    // IrisPrediction is the result returned from prediction operations
+    public class IrisPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabels;
+    }
+
+   public class Program
+    {
+        // STEP 1: Define your data structures
+
+        // IrisData is used to provide training data, and as 
+        // input for prediction operations
+        // - First 4 properties are inputs/features used to predict the label
+        // - Label is what you are predicting, and is only set when training
+        static void Main(string[] args)
+        {
+            // STEP 2: Create a pipeline and load your data
+            var pipeline = new LearningPipeline();
+
+            // If working in Visual Studio, make sure the 'Copy to Output Directory' 
+            // property of iris-data.txt is set to 'Copy always'
+            string dataPath = "data.txt";
+            pipeline.Add(new TextLoader(dataPath).CreateFrom<IrisData>(separator: ','));
+
+            // STEP 3: Transform your data
+            // Assign numeric values to text in the "Label" column, because only
+            // numbers can be processed during model training
+            pipeline.Add(new Dictionarizer("Label"));
+
+            // Puts all features into a vector
+            pipeline.Add(new TextFeaturizer("Features", "Name"));
+
+            // STEP 4: Add learner
+            // Add a learning algorithm to the pipeline. 
+            // This is a classification scenario (What type of iris is this?)
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+
+            // Convert the Label back into original text (after converting to number in step 3)
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
+
+            // STEP 5: Train your model based on the data set
+            var model = pipeline.Train<IrisData, IrisPrediction>();
+            
+            // STEP 6: Use your model to make a prediction
+            // You can change these numbers to test different predictions
+            var prediction = model.Predict(new IrisData()
+            {
+                Name= "数学在生活中的应用"
+            });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+            Console.ReadKey();
+        }
+    }
+}
